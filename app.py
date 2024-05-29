e29dffb32fff322b1b3f175893166e0d3c33b802
debg48 <debgandhar4000@gmail.com>
2024-05-30 01:58:55 +0530

    appv1

---
+++ app.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, File, UploadFile
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from PIL import Image
+import numpy as np
+import io
+# import logging
+
+app = FastAPI()
+
+# logging.basicConfig(level=logging.INFO)
+
+# Load your model
+model = load_model("content\my_model")
+
+
+
+@app.post("/predict/")
+async def predict(file: UploadFile ):
+    # logging.info(f"Received file: {file.filename}")
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    image = image.resize((240, 240))  # Resize to 240x240
+
+    # Preprocess the image
+    img_array = img_to_array(image)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # Make a prediction
+    prediction = np.argmax(model.predict(img_array))
+    # logging.info(f"Prediction: {prediction}")
+    # if prediction
+    if int(prediction) ==1 :
+        result = 'Not_Alzheimer'
+    else : 
+        result = 'Alzheimer'
+
+    return {"prediction": result}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
